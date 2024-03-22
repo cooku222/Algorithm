eb7ac3961f5572a12a9cdb027b9c72b7775d4337
cooku222 <68459662+cooku222@users.noreply.github.com>
2024-03-22 15:28:53 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; //Scanner를 사용하기 위해선 import문이 필요하다
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int A, B;
+        A=sc.nextInt();
+        B=sc.nextInt();
+        System.out.println(A+B);
+    }
+}
