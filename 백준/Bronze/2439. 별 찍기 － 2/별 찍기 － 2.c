4d0a3a82ebcd1fb33e73dee2b2611cb3cd06c5d0
cooku222 <68459662+cooku222@users.noreply.github.com>
2024-03-25 14:48:10 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int input;
+    scanf("%d", &input);
+    
+    for(int i=1; i<=input; i++){
+        for(int j=1; j<=input-i; j++)
+            printf(" ");
+        for(int k=1; k<=i; k++)
+            printf("*");
+        printf("\n");
+    }
+   return 0;
+}
