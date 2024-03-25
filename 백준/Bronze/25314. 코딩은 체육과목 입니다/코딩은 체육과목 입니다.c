3fc39e38184c025ac24f97816b300328f12c3013
cooku222 <68459662+cooku222@users.noreply.github.com>
2024-03-25 14:09:19 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    int l = n / 4;
+    for(int i=0;i<l;i++){
+        printf("long ");
+    }
+    printf("int");
+    
+    return 0;
+}
