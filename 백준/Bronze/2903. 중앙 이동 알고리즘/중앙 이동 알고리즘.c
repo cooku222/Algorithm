5eaa3080d3b92d28e88242fb50143dd2c5705c9a
cooku222 <68459662+cooku222@users.noreply.github.com>
2024-03-30 12:23:56 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n, i, k=2, sum=3;
+    scanf("%d", &n);
+    for(i=1;i<n;i++){
+        sum+=k;
+        k*=2;
+    }
+    printf("%d", sum*sum);
+    return 0;
+}
